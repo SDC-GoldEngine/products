a974e87985546368657a742b3dd90836f6688525
skydodle <skydodle@gmail.com>
2022-06-10 19:38:54 -0700

    models and controllers changed error handled method from console.log to throw

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -8,27 +8,27 @@ module.exports = {
     const { page, count } = req.query;
     readAll(page, count)
       .then((result) => { res.send(result).status(200); })
-      .catch((err) => { console.log(err); res.sendStatus(500); });
+      .catch((err) => { res.sendStatus(500); throw (err); });
   },
 
   getProduct: (req, res) => {
     const { product_id } = req.params;
     readProduct(product_id)
       .then((result) => { res.send(result).status(200); })
-      .catch((err) => { console.log(err); res.sendStatus(500); });
+      .catch((err) => { res.sendStatus(500); throw (err); });
   },
 
   getStyle: (req, res) => {
     const { product_id } = req.params;
     readStyle(product_id)
       .then((result) => { res.send(result).status(200); })
-      .catch((err) => { console.log(err); res.sendStatus(500); });
+      .catch((err) => { res.sendStatus(500); throw (err); });
   },
 
   getRelated: (req, res) => {
     const { product_id } = req.params;
     readRelated(product_id)
       .then((result) => { res.send(result).status(200); })
-      .catch((err) => { console.log(err); res.sendStatus(500); });
+      .catch((err) => { res.sendStatus(500); throw (err); });
   },
 };
