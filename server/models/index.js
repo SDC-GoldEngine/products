616153b28c64d54bad765f3abf76c140d8a5f69e
skydodle <skydodle@gmail.com>
2022-06-10 22:10:56 -0700

    Completed all queries in model/index.js; Tested with Postman. Fixed query missing path in routes.js.

--- server/models/index.js
+++ server/models/index.js
@@ -23,9 +23,43 @@ module.exports = {
       .catch((err) => { throw err; })
   ),
 
-  // readStyles: (productId) => (
-  //   pool.query(
-  //     `SELECT `
-  //   )
-  // ),
+  readStyle: (productId) => (
+    pool.query(`SELECT s.product_id AS product_id,
+      json_agg(
+        json_build_object(
+          'style_id', s.id, 'name', s.name, 'original_price', s.original_price,
+          'sale_price', s.sale_price, 'default?', s.default_style,
+          'photos', (
+            SELECT json_agg(
+              json_build_object(
+                'thumbnail_url', photos.thumbnail_url,
+                'url', photos.url
+              )
+            ) AS photos_arr
+            FROM photos WHERE photos.style_id = s.id
+            GROUP BY style_id
+          ),
+          'skus', (
+            SELECT json_object_agg(
+              skus.id::TEXT, json_build_object(
+                'quantity', skus.quantity,
+                'size', skus.size
+              )
+            ) AS skus_obj
+            FROM skus WHERE skus.style_id = s.id
+            GROUP BY style_id
+          )
+        )
+      ) AS results FROM styles s WHERE s.product_id = $1
+      GROUP BY s.product_id`, [productId])
+      .then((res) => res.rows[0])
+      .catch((err) => { throw err; })
+  ),
+
+  readRelated: (productId) => (
+    pool.query(`SELECT json_agg(related_product_id) FROM related
+    WHERE related.current_product_id = $1`, [productId])
+      .then((res) => res.rows[0].json_agg)
+      .catch((err) => { throw err; })
+  ),
 };
