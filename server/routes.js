4f6b86bfd6ab47a2dcbe5b014f003fd1eaa5a0d8
skydodle <skydodle@gmail.com>
2022-06-13 11:10:13 -0700

    modularized app; entry on index.js

--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,9 @@ const {
   getAll, getProduct, getStyle, getRelated,
 } = require('./controllers/index');
 
-router.get('/', getAll);
-router.get('/:product_id', getProduct);
-router.get('/:product_id/styles', getStyle);
-router.get('/:product_id/related', getRelated);
+router.get('/products', getAll);
+router.get('/products/:product_id', getProduct);
+router.get('/products/:product_id/styles', getStyle);
+router.get('/products/:product_id/related', getRelated);
 
 module.exports = router;
