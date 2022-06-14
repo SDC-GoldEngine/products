3e4ea16931cd10e88059993148653ae0f59f4d35
skydodle <skydodle@gmail.com>
2022-06-14 14:50:52 -0700

    added index for query performance

--- server/models/index.js
+++ server/models/index.js
@@ -12,13 +12,14 @@ module.exports = {
 
   readProduct: (productId) => (
     pool.query(`SELECT p.id, p.name, p.slogan, p.description, p.category, p.default_price,
-      json_agg(
-        json_build_object(
-          'feature', f.feature,
-          'value', f.value
-        )
-      ) AS features FROM products p JOIN features f ON p.id = f.product_id
-      WHERE p.id = $1 GROUP BY p.id ORDER BY p.id ASC`, [productId])
+    json_agg(
+      json_build_object(
+        'feature', f.feature,
+        'value', f.value
+      )
+    ) AS features
+    FROM products p LEFT JOIN features f ON p.id = f.product_id
+    WHERE p.id = $1 GROUP BY p.id`, [productId])
       .then((res) => res.rows[0])
       .catch((err) => { throw err; })
   ),
@@ -63,3 +64,14 @@ module.exports = {
       .catch((err) => { throw err; })
   ),
 };
+
+// LOOK INTO INDEXING
+// INDEX ON THE COLUMNS IM JOINING
+
+// readProduct: (productId) => (
+//   pool.query(`SELECT p.id, p.name, p.slogan, p.description, p.category, p.default_price,
+//   f.feature, f.value
+//   FROM products p LEFT JOIN features f ON p.id = f.product_id WHERE p.id = ${productId};`)
+//     .then((res) => res.rows[0])
+//     .catch((err) => { throw err; })
+// ),
--- server/psqlSetup.sql
+++ server/psqlSetup.sql
@@ -4,7 +4,8 @@ CREATE DATABASE productsdb;
 
 \c productsdb;
 
-CREATE TABLE IF NOT EXISTS products (
+DROP TABLE IF EXISTS products CASCADE;
+CREATE TABLE products (
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   name TEXT NOT NULL,
   slogan TEXT NOT NULL,
@@ -13,39 +14,44 @@ CREATE TABLE IF NOT EXISTS products (
   default_price INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS features(
+DROP TABLE IF EXISTS features CASCADE;
+CREATE TABLE features(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  product_id INT NOT NULL REFERENCES products ON DELETE CASCADE,
+  product_id INT NOT NULL REFERENCES products(id),
   feature TEXT NOT NULL,
   value TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS styles(
+DROP TABLE IF EXISTS styles CASCADE;
+CREATE TABLE styles(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  product_id INT NOT NULL REFERENCES products ON DELETE CASCADE,
+  product_id INT NOT NULL REFERENCES products(id),
   name TEXT NOT NULL,
   sale_price TEXT,
   original_price TEXT,
   default_style BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE IF NOT EXISTS skus(
+DROP TABLE IF EXISTS skus CASCADE;
+CREATE TABLE skus(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  style_id INT NOT NULL REFERENCES styles ON DELETE CASCADE,
+  style_id INT NOT NULL REFERENCES styles(id),
   size TEXT NOT NULL,
   quantity INT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS photos(
+DROP TABLE IF EXISTS photos CASCADE;
+CREATE TABLE photos(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  style_id INT NOT NULL REFERENCES styles ON DELETE CASCADE,
+  style_id INT NOT NULL REFERENCES styles(id),
   url TEXT,
   thumbnail_url TEXT
 );
 
-CREATE TABLE IF NOT EXISTS related(
+DROP TABLE IF EXISTS related CASCADE;
+CREATE TABLE related(
   id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  current_product_id INT NOT NULL REFERENCES products ON DELETE CASCADE,
+  current_product_id INT NOT NULL REFERENCES products(id),
   related_product_id INT NOT NULL
 );
 
@@ -72,3 +78,10 @@ WITH CSV HEADER;
 COPY related (id, current_product_id, related_product_id)
 FROM '/Users/johnnywu/Documents/SDC/Data/related.csv'
 WITH CSV HEADER;
+
+CREATE INDEX products_id_index ON products (id);
+CREATE INDEX features_product_id ON features (product_id);
+CREATE INDEX styles_index ON styles (product_id);
+CREATE INDEX photos_index ON photos (style_id);
+CREATE INDEX skus_index ON skus (style_id);
+CREATE INDEX related_index ON related (current_product_id);
